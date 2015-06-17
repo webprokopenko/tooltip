a65fa18d8d1a8e4a4158e4c07ae4eb5dc0553f9f
Ivan Prokopenko <web_prokopenko@gmail.com>
2015-06-17 21:40:57 +0400

    tooltip js done, css - round not down

--- app/js/main.js
+++ app/js/main.js
@@ -1,104 +1,67 @@
-// "use strict";
-// var tooltip = (function(){
-// 	var setUpListeners = function(){
-// 		$('form').on('submit',tooltip);
-// 	},
-// 	tooltip = function(e){
-// 		e.preventDefault();
-
-// 		var elems = $('form').find('input[tooltip],textarea[tooltip]');
-// 		console.log(elems);
-
-// 	};
-// 	return{
-// 		init: function(){
-// 			setUpListeners();
-// 		}
-// 	}
-// }());
-
-// tooltip.init();
-
-$(document).ready(function(){
-	$('form').on('submit', function(e){
+"use strict";
+var tooltip = (function(){
+	var setUpListeners = function(){
+		$('form').on('submit',tooltip);
+	},
+	tooltip = function(e){
 		e.preventDefault();
-		$('.test').tooltip({
-			position: 'top',
-			contetnt: 'adfasdfasdf'
-		})
-	})
-});
 
+		var $this = this,
+            body = $('body'),
+            elems = $('form').find('input[tooltip],textarea[tooltip]');
 
-$.fn.tooltip = function(options){
-	options = {
-		position	: options.position || 'right',
-		content		: options.content || 'I am tooltip' 
-	};
-	var
-		markup = '<div class="tooltip tooltip_'+options.position+'">\
-           <div class="tooltip_inner">'+options.content+'</div>\
-       </div>';
-    var
-    	$this = this,
-    	body = $('body');
+        for (var i=0, max=elems.length; i < max; i+=1) {
+                console.log($(elems[i]));
+                 var markup = '<div class="tooltip tooltip_'+$(elems[i]).attr('tooltip')+'">\
+                            <div class="tooltip_inner">'+$(elems[i]).attr('tooltip-content')+'</div>\
+                            </div>';
 
-    $this
-    	.addClass('tooltipstered')
-    	.attr('data-tooltip-position', options.position);
+                body.append(markup);
+                position_elem($(elems[i]), body.find('.tooltip').last(), $(elems[i]).attr('tooltip'));
+            };    
+	},
+    position_elem = function(elem,tooltip,position){
+        // определяем положение элемента к которому будет привязан тултип
+        var elemWidth   = elem.outerWidth(true),
+            elemHeight  = elem.outerHeight(true),
+            topEdge     = elem.offset().top,
+            leftEdge    = elem.offset().left,
+            bottomEdge  = topEdge + elemHeight;
+        // определяем размеры самого тултипа
+        var tooltipWidth    = tooltip.outerWidth(true),
+            tooltipHeight   = tooltip.outerHeight(true);
 
-    body.append(markup);
-    positionIt($this, body.find('.tooltip').last(), options.position);
+        if (position==='left') {
+            position={
+                left:(leftEdge-tooltipWidth),
+                top:topEdge
+            }
+        }else if(position==='right'){
+            position={
+                left:(leftEdge+elemWidth),
+                top:topEdge
+            }
+        }else if(position ==='top'){
+            position={
+                left:((leftEdge) + ((elemWidth/2) - (tooltipWidth/2))),
+                top:((topEdge-tooltipHeight)-6)
+            }
+        }else if(position ==='bottom'){
+            position={
+                left:(leftEdge) + ((elemWidth/2) - (tooltipWidth/2)),
+                top:(topEdge+tooltipHeight)
+            }
+        }
+        console.log(position);
 
-    function positionIt(elem, tooltip, position){
-    	//измеряем элемент
+        tooltip.offset(position).css('opacity','1');
 
-    	var
-    		elemWidth 	= elem.outerWidth(true),
-    		elemHeight 	= elem.outerHeight(true),
-    		topEdge 	= elem.offset().top,
-    		bottmEgde 	= topEdge + elemHeight;
-    		leftEdge 	= elem.offset().top,
-    		rightEdge	= leftEdge + elemWidth;
+    };
+	return{
+		init: function(){
+			setUpListeners();
+		}
+	}
+}());
 
-    	// изменяем тултип
-
-    	var 
-    		tooltipWidth 	= tooltip.outerWidth(true),
-    		tooltipHeight 	= tooltip.outerHeight(true),
-    		leftCentered	= (elemWidth / 2) - (tooltipWidth / 2),
-    		topCentered		= (elemHeight / 2) - (tooltipHeight / 2);
-
-    	// var position={};
-    	
-    	switch (position){
-    		case 'right':
-    			position = {
-    				left: rightEdge,
-    				top: topEdge + topCentered
-    			};
-    			break;
-    		case 'top':
-    			position = {
-    				left: leftEdge + leftCentered,
-    				top: topEdge - tooltipHeight
-    			};
-    			break;
-    		case 'left':
-    			position = {
-    				left: leftEdge + leftCentered,
-    				top: bottmEgde
-    			};
-    			break;
-    		case 'bottom':
-    			position = {
-    				left: leftEdge - tooltipWidth,
-    				top: topEdge + topCentered
-    			};	
-    			break;
-    	};
-
-    	tooltip.offset(position).css('opacity', '1');
-    	console.log(position);
-    }
-};
+tooltip.init();
